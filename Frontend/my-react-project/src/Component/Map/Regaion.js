185cb922aa243e546302adfabfbeff6a511d90c7
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2023-04-02 10:36:08 +0700

    uodate howpage and little adjust color of region

--- Frontend/my-react-project/src/Component/Map/Regaion.js
+++ Frontend/my-react-project/src/Component/Map/Regaion.js
@@ -1,57 +1,57 @@
 import React, { useState, useEffect } from "react";
-import Icebox from '../../region@2x.png'
-import './regaion.css'
+import Icebox from "../../region@2x.png";
+import "./regaion.css";
 
 function Grid({ matrix, W, H, onGridClick }) {
-    const imageName = { Icebox };
-  
-    const handleClick = (rowIndex, cellIndex) => {
-      if (onGridClick) {
-        onGridClick(rowIndex, cellIndex);
-      }
-    };  
-   
-    return (
-        <>
-          {matrix.map((row, rowIndex) => (
-            <span99
-              className={rowIndex % 2 === 0 ? "odd" : "even"}
-              style={{ width: W - W / 4 + 3 }}
-              key={rowIndex}
-            >
-              <div>
-                {row.map((cell, cellIndex) => (
-                  <div key={cellIndex}>
-                    <img
-                      key={cellIndex}
-                      src={Icebox}
-                      alt=""
-                      width={W}
-                      height={H}
-                      onError={() => console.log(`Error loading ${imageName}`)}
-                      style={{ flex: 1 }}
-                      onClick={() => handleClick(rowIndex, cellIndex)}
-                      draggable="false"
-                    />
-                  </div>
-                ))}
-              </div>
-            </span99>
-          ))}
-        </>
-      );
-    }
+  const imageName = { Icebox };
 
-    export default function GridContainer() {
-        const rows = 11;
-        const columns = 9;
-        const H = 82;
-        const W = (246 / 212) * H;
-      
-        const matrix = new Array(rows);
-        for (let i = 0; i < rows; i++) {
-          matrix[i] = new Array(columns).fill(null);
-        }
+  const handleClick = (rowIndex, cellIndex) => {
+    if (onGridClick) {
+      onGridClick(rowIndex, cellIndex);
+    }
+  };
+
+  return (
+    <>
+      {matrix.map((row, rowIndex) => (
+        <span99
+          className={rowIndex % 2 === 0 ? "odd" : "even"}
+          style={{ width: W - W / 4 + 3 }}
+          key={rowIndex}
+        >
+          <div>
+            {row.map((cell, cellIndex) => (
+              <div key={cellIndex}>
+                <img
+                  key={cellIndex}
+                  src={Icebox}
+                  alt=""
+                  width={W}
+                  height={H}
+                  onError={() => console.log(`Error loading ${imageName}`)}
+                  style={{ flex: 1 }}
+                  onClick={() => handleClick(rowIndex, cellIndex)}
+                  draggable="false"
+                />
+              </div>
+            ))}
+          </div>
+        </span99>
+      ))}
+    </>
+  );
+}
+
+export default function GridContainer() {
+  const rows = 11;
+  const columns = 9;
+  const H = 82;
+  const W = (246 / 212) * H;
+
+  const matrix = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    matrix[i] = new Array(columns).fill(null);
+  }
 
   // Define an array of image names
   const imageNames = Array.from({ length: rows * columns }, (_, i) => {
@@ -71,7 +71,7 @@ function Grid({ matrix, W, H, onGridClick }) {
       imageIndex++;
     }
   }
-  
+
   const [zoomLevel, setZoomLevel] = useState(1);
   const [isDragging, setIsDragging] = useState(false);
   const [lastMousePosition, setLastMousePosition] = useState({ x: 0, y: 0 });
@@ -100,7 +100,6 @@ function Grid({ matrix, W, H, onGridClick }) {
     };
   }, [zoomLevel]);
 
-
   const [clickedCell, setClickedCell] = useState(null);
   const handleGridClick = (row, column) => {
     setClickedCell({ row, column });
@@ -108,26 +107,32 @@ function Grid({ matrix, W, H, onGridClick }) {
 
   return (
     <>
-    <div  className="buttonszoom">
-
-      <button onClick={() => setZoomLevel(zoomLevel + 0.1)}>Zoom In</button>
-      <button onClick={() => setZoomLevel(zoomLevel - 0.1)}>Zoom Out</button>
-      <button onClick={() => {
-        setZoomLevel(1);
-        setContainerPosition({ x: 0, y: 0 });
-      }}>Reset</button>
-    </div>
-
-    <div style={{overflow: "hidden"}} className="container4">
-      <div style={{transform: transformStyle}} className="grid-container">
-      <Grid matrix={matrix} W={W} H={H} onGridClick={handleGridClick} />
+      <div className="buttonszoom">
+        <button onClick={() => setZoomLevel(zoomLevel + 0.1)}>Zoom In</button>
+        <button onClick={() => setZoomLevel(zoomLevel - 0.1)}>Zoom Out</button>
+        <button
+          onClick={() => {
+            setZoomLevel(1);
+            setContainerPosition({ x: 0, y: 0 });
+          }}
+        >
+          Reset
+        </button>
       </div>
-    </div >
-    {clickedCell && (
+
+      <div style={{ overflow: "hidden" }} className="container4">
+        <div style={{ transform: transformStyle }} className="grid-container">
+          <Grid matrix={matrix} W={W} H={H} onGridClick={handleGridClick} />
+        </div>
+      </div>
+      {clickedCell && (
         <p className="text">
-          R {clickedCell.row + 1} , C {clickedCell.column + 1} 
-          </p>
+          R {clickedCell.row + 1} , C {clickedCell.column + 1}
+        </p>
       )}
+      <div>
+        <p className="text2">AAA</p>
+      </div>
     </>
   );
 }
--- Frontend/my-react-project/src/pages/howpage/Howpage.js
+++ Frontend/my-react-project/src/pages/howpage/Howpage.js
@@ -1,18 +1,23 @@
 // eslint-disable-next-line
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './howpage.css';
-import logo from '../../logo1.png'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./howpage.css";
+import logo from "../../logo1.png";
+import How from "./How.png";
 
 function Howpage() {
   return (
     <div>
-    <div className="bg-image"></div>
-    <div className="square"></div>
-    <img id="logo"src={logo} />
-
-    <div id="box1"></div>
-    <button><a href="#" className="custom-btn btn-11"><span2>NEXT</span2></a></button>
+      <div className="bg-image"></div>
+      <div className="square"></div>
+      <img id="logo" src={logo} />
+      <img id="How" src={How} />
+      <div id="box1"></div>
+      <button>
+        <a href="#" className="custom-btn btn-11">
+          <span2>NEXT</span2>
+        </a>
+      </button>
     </div>
   );
 }
